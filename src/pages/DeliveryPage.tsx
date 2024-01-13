2515c90d0a0a8ff2b35f9ef75cbd72db87f837d2
Sergio Botter <sergio.botter@gmail.com>
2024-01-12 22:59:11 -0800

    refactoring delivery page

--- src/pages/DeliveryPage.tsx
+++ src/pages/DeliveryPage.tsx
@@ -1,38 +1,34 @@
-import {
-  Heading,
-  Text,
-  Box,
-  Card,
-  CardBody,
-  CardHeader,
-} from "@chakra-ui/react";
+import { Heading, Text, Flex, VStack } from "@chakra-ui/react";
 import Footer from "../components/Footer";
 import Map from "../components/Map";
 
 const DeliveryPage = () => {
   return (
     <>
-      <Box padding={5} boxSize={"100%"}>
-        <Card borderRadius={20} bg="#FFF" boxShadow="md">
-          <CardHeader>
-            <Heading marginTop={10} marginBottom={10}>
-              <Text
-                color="base.700"
-                fontSize={{ base: "30px", md: "50px", lg: "80px" }}
-                align="center"
-              >
-                WE DELIVERY!
-              </Text>
-            </Heading>
-          </CardHeader>
-          <CardBody>
-            <Box boxSize={"100%"}>
-              <Map />
-            </Box>
-          </CardBody>
-        </Card>
-      </Box>
-
+      <VStack>
+        <Heading marginTop={10} marginBottom={10}>
+          <Text
+            color="base.700"
+            fontSize={{ base: "30px", md: "50px", lg: "80px" }}
+            align="center"
+          >
+            WE DELIVERY!
+          </Text>
+        </Heading>
+        <Text
+          color="base.700"
+          fontSize={{ base: "15px", md: "25px", lg: "40px" }}
+          align="center"
+        >
+          Experience the convenience of culinary delight at your doorstep with
+          our delivery service bringing the flavors of artisanal excellence
+          straight to you, ensuring every bite is a journey of freshness and
+          satisfaction.
+        </Text>
+        <Flex width="100%" justify="center">
+          <Map />
+        </Flex>
+      </VStack>
       <Footer />
     </>
   );
