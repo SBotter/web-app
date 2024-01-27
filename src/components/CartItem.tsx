54fc58619a4e060042c9142d8038b68cdd790eb3
Sergio Botter <sergio.botter@gmail.com>
2024-01-26 17:06:08 -0800

    fixed Layout carrinho no mobile

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,4 +1,14 @@
-import { Box, HStack, VStack, Image, Text, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  HStack,
+  VStack,
+  Image,
+  Text,
+  Flex,
+  SimpleGrid,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
 import { QuantitySelector } from "./QuantitySelector";
@@ -59,17 +69,45 @@ export function CartItem(cartItem: CartItemProps) {
   }
 
   return (
-    <VStack paddingBottom={5}>
-      <HStack
+    <Box
+      width={"100%"}
+      padding={2}
+      borderRadius={20}
+      borderWidth={1}
+      borderColor={"base.800"}
+      bg="#FFF"
+      marginBottom={2}
+    >
+      <Grid
         width={"100%"}
-        padding={2}
-        borderRadius={20}
-        borderWidth={1}
-        borderColor={"base.800"}
-        bg="#FFF"
+        templateAreas={`"title title"
+                  "img price"
+                  "img cart"`}
+        gridTemplateRows={"60px 1fr 60px"}
+        gridTemplateColumns={"100px 1fr"}
+        h="180px"
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
       >
-        <Box>
+        <GridItem pl={2} bg="#FFF" area={"title"}>
+          <VStack align="start">
+            <Text
+              color="base.800"
+              fontSize={20}
+              fontWeight={"bold"}
+              marginTop={-1}
+            >
+              {name}
+            </Text>
+            <Text color="base.800" fontSize={13} marginTop={-6}>
+              {quantity} {packageUnit} {packageSize}
+            </Text>
+          </VStack>
+        </GridItem>
+        <GridItem bg="#FFF" area={"img"}>
           <Box
+            marginTop={2}
             borderRadius="full"
             borderWidth={1}
             borderColor={"#base.800"}
@@ -78,68 +116,48 @@ export function CartItem(cartItem: CartItemProps) {
             <Box borderRadius="full" borderWidth={5} borderColor={"#FFF"}>
               <Image
                 src={`/images/products/${imageSrc}`}
-                boxSize={"80px"}
+                boxSize={"90px"}
                 borderRadius="full"
                 objectFit={"cover"}
               />
             </Box>
           </Box>
-        </Box>
-        <Box marginLeft={5} width={"60%"}>
-          <VStack align="start">
-            <Text
-              color="base.800"
-              fontSize={20}
-              fontWeight={"bold"}
-              marginTop={3}
-            >
-              {name}
-            </Text>
-            <Text color="base.800" fontSize={15} marginTop={-5}>
-              {quantity} {packageUnit} {packageSize}
-            </Text>
-          </VStack>
-        </Box>
-        <Box>
-          <VStack>
-            <HStack align="start">
-              <Box marginTop={4}>
-                <i className="fa-solid fa-dollar-sign product-detail-icon-link" />
-              </Box>
-              <Box>
-                <Text
-                  color="base.800"
-                  fontSize={20}
-                  fontWeight={"bold"}
-                  marginTop={3}
-                >
-                  {totalPriceFromItem}
-                </Text>
+        </GridItem>
+        <GridItem bg="#FFF" area={"price"}>
+          <SimpleGrid columns={2} justifyContent={"space-around"}>
+            <Flex justifyContent={"right"} marginTop={3}>
+              <i className="fa-solid fa-dollar-sign product-detail-icon-link" />
+            </Flex>
+            <Flex justifyContent={"left"} marginTop={1}>
+              <Text color="base.800" fontSize={25} fontWeight={"bold"}>
+                {totalPriceFromItem}
+              </Text>
+            </Flex>
+          </SimpleGrid>
+        </GridItem>
+        <GridItem bg="#FFF" area={"cart"}>
+          <Flex
+            borderColor={"black"}
+            justifyContent={"space-evenly"}
+            textAlign={"center"}
+            verticalAlign={"center"}
+          >
+            <HStack>
+              <QuantitySelector
+                quantity={quantity}
+                onMinusButtonClicked={() => handleMinusButtonClicked()}
+                onPlusButtonClicked={() => handlePlusButtonClicked()}
+              />
+              <Box marginTop={-1}>
+                <i
+                  className="fa-solid fa-trash product-detail-icon-link"
+                  onClick={() => handleRemoveItemButtonClicked()}
+                />
               </Box>
             </HStack>
-            <Flex
-              borderColor={"black"}
-              justifyContent={"space-evenly"}
-              textAlign={"center"}
-              verticalAlign={"center"}
-            >
-              <HStack>
-                <QuantitySelector
-                  quantity={quantity}
-                  onMinusButtonClicked={() => handleMinusButtonClicked()}
-                  onPlusButtonClicked={() => handlePlusButtonClicked()}
-                />
-                <Box marginTop={-1}>
-                  <i
-                    className="fa-solid fa-trash product-detail-icon-link"
-                    onClick={() => handleRemoveItemButtonClicked()}
-                  />
-                </Box>
-              </HStack>
-            </Flex>
-          </VStack>
-        </Box>
-      </HStack>
-    </VStack>
+          </Flex>
+        </GridItem>
+      </Grid>
+    </Box>
   );
 }
