dd0744d85642b250303ef0741c76dd202caac141
Sergio Botter <sergio.botter@gmail.com>
2024-01-27 12:39:09 -0800

    Fix navigation state issue

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import { Box } from "@chakra-ui/react";
 import Header from "../components/Header";
+import { ScrollRestoration } from "react-router-dom";
 
 const Layout = () => {
   return (
     <>
+      <ScrollRestoration />
       <Header />
       <Box bg="base.50">
         <Outlet />
