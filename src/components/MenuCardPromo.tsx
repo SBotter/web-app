86da7803253d8c9466344446fcf268b0074b5b2a
Sergio Botter <sergio.botter@gmail.com>
2024-01-27 20:54:17 -0800

    Refactoring promo card

--- src/components/MenuCardPromo.tsx
+++ src/components/MenuCardPromo.tsx
@@ -17,7 +17,7 @@ interface Props {
 const MenuCardPromo = ({ product }: Props) => {
   return (
     <>
-      <Card borderRadius={20} bg="#FFF" boxShadow="md" height={"350px"}>
+      <Card borderRadius={20} bg="#FFF" boxShadow="md" height={"100%"}>
         <CardHeader p={2}>
           <Image
             src={`/images/products/${product.picture}`}
@@ -28,19 +28,22 @@ const MenuCardPromo = ({ product }: Props) => {
             borderTopRightRadius={20}
             overflow="hidden"
           />
-          <Text color="base.800">{product.productGroupName}</Text>
-          <Divider backgroundColor={"base.800"} width="100%" />
+          <Text color="base.800" fontSize="15px" fontWeight="bold">
+            {product.productGroupName}
+          </Text>
+          <Divider backgroundColor={"base.800"} width="100%" marginTop={-2} />
         </CardHeader>
-        <CardBody marginTop={-8} p={0}>
+        <CardBody p={0}>
           <SimpleGrid columns={1} spacing={2} p={2}>
             {product.package.map((prod) => (
               <Box
                 key={prod.packageId}
                 textAlign={"left"}
                 height={{ base: "45px" }}
+                marginTop={-7}
               >
                 <Text color={"base.800"}>
-                  {prod.packageSize} - {prod.packageName}
+                  {prod.packageName} - {prod.packageSize}
                 </Text>
               </Box>
             ))}
--- src/components/MenuPromoList.tsx
+++ src/components/MenuPromoList.tsx
@@ -6,6 +6,12 @@ import {
   Box,
   CardFooter,
   CardHeader,
+  useBreakpointValue,
+  CardBody,
+  VStack,
+  Divider,
+  Flex,
+  HStack,
 } from "@chakra-ui/react";
 
 import useProductsPromotional from "../hooks/useProductsPromotional";
@@ -13,6 +19,7 @@ import MenuCardPromo from "./MenuCardPromo";
 
 const MenuPromoList = () => {
   const { data } = useProductsPromotional();
+  const displayMode = useBreakpointValue({ base: "text", md: "menu" });
   return (
     <>
       <Box p={5}>
@@ -29,16 +36,87 @@ const MenuPromoList = () => {
             </Heading>
           </CardHeader>
           <CardFooter justifyContent={"space-between"}>
-            <SimpleGrid
-              columns={{ base: 1, sm: 1, md: 4, lg: 4, xl: 4 }}
-              spacing={{ base: 1, sm: 3, md: 4, lg: 4, xl: 4 }}
-              p={{ base: 1, sm: 3, md: 4, lg: 4, xl: 4 }}
-              width={"100%"}
-            >
-              {data.map((prod) => (
-                <MenuCardPromo key={prod.productId} product={prod} />
-              ))}
-            </SimpleGrid>
+            {displayMode === "menu" ? (
+              <SimpleGrid
+                columns={{ base: 1, sm: 1, md: 4, lg: 4, xl: 4 }}
+                spacing={{ base: 1, sm: 3, md: 4, lg: 4, xl: 4 }}
+                p={{ base: 1, sm: 3, md: 4, lg: 4, xl: 4 }}
+                width={"100%"}
+              >
+                {data.map((prod) => (
+                  <MenuCardPromo key={prod.productId} product={prod} />
+                ))}
+              </SimpleGrid>
+            ) : (
+              <>
+                <Card
+                  borderRadius={20}
+                  bg="#FFF"
+                  boxShadow="md"
+                  width={"100%"}
+                  marginTop={-8}
+                >
+                  <CardBody width={"100%"}>
+                    {data.map((prod) => (
+                      <Box key={prod.productId} width={"100%"}>
+                        <VStack>
+                          <Text
+                            fontSize="15px"
+                            fontWeight="bold"
+                            color="base.800"
+                            verticalAlign={"left"}
+                          >
+                            {prod.productGroupName}
+                          </Text>
+                          {prod.package.map((pkg) => (
+                            <>
+                              <Flex width={"100%"} marginTop={-7}>
+                                <HStack
+                                  width={"100%"}
+                                  justifyContent={"center"}
+                                >
+                                  <Box
+                                    key="icon"
+                                    width={"10%"}
+                                    textAlign={"center"}
+                                  >
+                                    <i className="fa-regular fa-heart product-detail-icon-link" />
+                                  </Box>
+                                  <Box
+                                    key={pkg.packageName}
+                                    width={"70%"}
+                                    verticalAlign={"left"}
+                                    marginTop={2}
+                                  >
+                                    <Text color={"base.800"}>
+                                      {pkg.packageName}
+                                    </Text>
+                                  </Box>
+                                  <Box
+                                    key={pkg.packageName}
+                                    width={"20%"}
+                                    verticalAlign={"left"}
+                                  >
+                                    <Text color={"base.800"}>
+                                      {pkg.packageSize}
+                                    </Text>
+                                  </Box>
+                                </HStack>
+                              </Flex>
+                            </>
+                          ))}
+                          <Divider
+                            backgroundColor={"base.800"}
+                            width="100%"
+                            marginTop={-2}
+                          />
+                        </VStack>
+                      </Box>
+                    ))}
+                  </CardBody>
+                </Card>
+              </>
+            )}
           </CardFooter>
         </Card>
       </Box>
